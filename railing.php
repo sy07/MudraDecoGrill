2c5522993b9c86a50bedbdac2e191831d47ef928
sy07 <soniyash0007@gmail.com>
2021-07-02 12:35:50 +0530

    first commit

---
+++ railing.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <!-- Page Title -->
+  <title>Mudra</title>
+  <!-- / -->
+
+  <!---Font Icon-->
+  <link href="static/plugin/font-awesome/css/fontawesome-all.min.css" tppabs="http://adhyasoft.com/themeforest/dome/static/plugin/font-awesome/css/fontawesome-all.min.css" rel="stylesheet">
+  <link href="static/plugin/themify-icons/themify-icons.css" tppabs="http://adhyasoft.com/themeforest/dome/static/plugin/themify-icons/themify-icons.css" rel="stylesheet">
+  <!-- / -->
+
+  <!-- Plugin CSS -->
+  <link href="static/plugin/bootstrap/css/bootstrap.min.css" tppabs="http://adhyasoft.com/themeforest/dome/static/plugin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="static/plugin/owl-carousel/css/owl.carousel.min.css" tppabs="http://adhyasoft.com/themeforest/dome/static/plugin/owl-carousel/css/owl.carousel.min.css" rel="stylesheet">
+  <link href="static/plugin/magnific/magnific-popup.css" tppabs="http://adhyasoft.com/themeforest/dome/static/plugin/magnific/magnific-popup.css" rel="stylesheet">
+
+  <!-- / -->
+
+  <!-- Theme Style -->
+  <link href="static/css/styles.css" tppabs="http://adhyasoft.com/themeforest/dome/static/css/styles.css" rel="stylesheet">
+  <link href="static/css/color/default.css" tppabs="http://adhyasoft.com/themeforest/dome/static/css/color/default.css" rel="stylesheet" id="color_theme">
+  <!-- / -->
+<script src="https://kit.fontawesome.com/bb4960f35e.js"></script>
+  <!-- Favicon -->
+  <link rel="icon" href="favicon.ico" />
+  <!-- / -->
+  <style type="text/css">
+        
+.gallery-title
+{
+    font-size: 36px;
+    color: #42B32F;
+    text-align: center;
+    font-weight: 500;
+    margin-bottom: 70px;
+}
+.gallery-title:after {
+    content: "";
+    position: absolute;
+    width: 7.5%;
+    left: 46.5%;
+    height: 45px;
+    border-bottom: 1px solid #5e5e5e;
+}
+.filter-button
+{
+    font-size: 18px;
+    border: 1px solid #42B32F;
+    border-radius: 5px;
+    text-align: center;
+    color: #42B32F;
+    margin-bottom: 30px;
+
+}
+.filter-button:hover
+{
+    font-size: 18px;
+    border: 1px solid #42B32F;
+    border-radius: 5px;
+    text-align: center;
+    color: #ffffff;
+    background-color: #42B32F;
+
+}
+.btn-default:active .filter-button:active
+{
+    background-color: #42B32F;
+    color: white;
+}
+
+.port-image
+{
+    width: 100%;
+}
+
+.gallery_product
+{
+    margin-bottom: 30px;
+}
+
+    </style>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+
+<!-- Body Start -->
+<body data-spy="scroll" data-target="#navbar" data-offset="98">
+
+  
+  <!-- Header -->
+  <header>
+    <nav class="navbar header-nav fixed-top navbar-expand-lg">
+      <div class="container">
+        <!-- Brand -->
+        <a class="navbar-brand" href="index.php">
+          Mudra <span>Deco Grill</span>
+        </a>
+        <!-- / -->
+        <!-- Top Menu -->
+        
+         
+
+        <!-- / -->
+
+      </div><!-- Container -->
+    </nav> <!-- Navbar -->
+  </header>
+  <!-- Header End -->
+<main>
+  <section>
+  <div class="container"><br/><br/><br/><br/><br/>
+        <div class="row">
+        <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1 class="gallery-title">Gallery</h1>
+        </div>
+
+        
+        <br/>
+
+
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img3.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:WR01</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img21.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:WR02</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img39.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:WR03</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img42.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:WR04</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img54.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:WR05</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img60.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:WR06</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img12.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR01</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img13.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR02</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img19.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR03</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img22.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR04</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img24.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR05</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img28.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR06</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img43.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR07</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img47.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR08</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img53.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR09</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img94.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:GR10</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img1.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR01</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img11.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR04</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img16.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR05</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img30.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR06</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img34.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR07</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img50.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR08</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img51.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR09</h4></center>
+            </div>
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="static/img/photo/img95.jpg" class="img-responsive" height="365" width="365">
+                <center><h4>Model:PR10</h4></center>
+            </div>
+            
+        </div>
+    </div>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+</script>
+</section>
+</main>
+</html>
